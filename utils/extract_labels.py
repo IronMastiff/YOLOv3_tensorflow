71ea841c9aafd905ca1eb4ed4892eaca42fed0e5
Iron Mastiff <1760298283@qq.com>
2018-04-08 12:08:55 +0800

    Finish extract_data function

---
+++ utils/extract_labels.py
@@ -0,0 +1,35 @@
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+def extract_data( dir ):
+    DOMTree = parse( dir )
+    collection = DOMTree.documentElement
+    file_name_xml = collection.getElementsByTagName( 'filename' )[0]
+    objects_xml = collection.getElementsByTagName( 'object' )
+
+    file_name = file_name_xml.childNodes[0].data
+
+    objects = []
+    for object_xml in objects_xml:
+        object_name = object_xml.getElementsByTagName( 'name' )[0]
+        bdbox = object_xml.getElementsByTagName( 'bndbox' )[0]
+        xmin = bdbox.getElementsByTagName( 'xmin' )[0]
+        ymin = bdbox.getElementsByTagName( 'ymin' )[0]
+        xmax = bdbox.getElementsByTagName( 'xmax' )[0]
+        ymax = bdbox.getElementsByTagName( 'ymax' )[0]
+
+        object = ( object_name.childNodes[0].data,
+                   xmin.childNodes[0].data,
+                   ymin.childNodes[0].data,
+                   xmax.childNodes[0].data,
+                   ymax.childNodes[0].data )
+
+        objects.append( object )
+
+    return file_name, objects
+
+
+'''--------Test extract_labels--------'''
+if __name__ == '__main__':
+    file_name, objects = extract_data( '../data/VOCtest_06-Nov-2007/Annotations/000001.xml' )
+    print( file_name, '\n', objects )
