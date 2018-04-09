56f92425cadac55e0853cf8cf472b4fd1886d059
Iron Mastiff <1760298283@qq.com>
2018-04-09 15:07:11 +0800

    Fnish feature extractor

--- utils/net.py
+++ utils/net.py
@@ -2,12 +2,7 @@ from utils import extract_labels
 import tensorflow as tf
 import numpy as np
 import cv2
-
-def create_placeholder( inputs, labels ):
-    X = tf.placeholder( tf.float32, [None, input.shape[1], input.shape[2], 3] )
-    Y = tf.placeholder( tf.float32, [None, labels.shape[1], labels.shape[2], 3] )
-
-    return X, Y
+import matplotlib.pyplot as plt
 
 
 def Leaky_Relu( input, alpha = 0.01 ):
@@ -24,7 +19,7 @@ def conv2d( inputs, filters, shape, stride = ( 1, 1 ) ):
                               padding = 'SAME',
                               kernel_initializer=tf.truncated_normal_initializer(stddev=0.01) )
 
-    layer = tf.layers.batch_normalization( layer )
+    layer = tf.layers.batch_normalization( layer, training = True )
 
     layer = Leaky_Relu( layer )
 
@@ -39,7 +34,7 @@ def Res_conv2d( inputs, filters, shape, stride = ( 1, 1 ) ):
                               padding = 'SAME',
                               kernel_initializer = tf.truncated_normal_initializer( stddev = 0.01 ) )
 
-    layer = tf.layers.batch_normalization( layer )
+    layer = tf.layers.batch_normalization( layer, training = True )
 
     return layer
 
@@ -52,7 +47,7 @@ def Res( input, shortcut ):
 
 
 
-def net( inputs ):
+def feature_extractor( inputs ):
     layer = conv2d( inputs, 32, [3, 3] )
     layer = conv2d( layer, 64, [3, 3], ( 2, 2 ) )
     shortcut = layer
@@ -93,24 +88,26 @@ def net( inputs ):
         layer = Res_conv2d( layer, 1024, [3, 3] )
         layer = Res( layer, shortcut )
 
-    avg_pool = tf.nn.pool( layer, 'AVG', 'SAME' )
-    fc = tf.layers.dense( avg_pool, 1000 )
-    softmax = tf.nn.softmax( fc )
+    # avg_pool = tf.nn.avg_pool( layer, [1, 1, 1, 1], [1, 1, 1, 1], padding = 'SAME' )
+    # fc = tf.layers.dense( avg_pool, 1000 )
+    # softmax = tf.nn.softmax( fc )
 
-    return layer,  softmax
+    return layer
 
 '''--------Test the net--------'''
 if __name__ == "__main__":
     data = cv2.imread(  '../data/VOCtest_06-Nov-2007/JPEGImages/000001.jpg' )
     data = cv2.cvtColor( data, cv2.COLOR_BGR2RGB )
+    data = cv2.resize( data, ( 512, 512 ) )
 
     data = tf.cast( tf.expand_dims( tf.constant( data ), 0 ), tf.float32 )
 
-    layer, softmax = net(data)
+    layer = feature_extractor( data )
 
     with tf.Session() as sess:
 
         sess.run( tf.initialize_all_variables() )
 
-        print( sess.run( softmax ).shape )
+        print( sess.run( layer ).shape )
+
 
