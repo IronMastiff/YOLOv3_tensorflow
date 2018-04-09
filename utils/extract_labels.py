92c579e4de258d77a61881187c01b3bf752e3d5a
Iron Mastiff <1760298283@qq.com>
2018-04-09 15:16:39 +0800

    Finished the xml_extractor

--- utils/extract_labels.py
+++ utils/extract_labels.py
@@ -1,13 +1,21 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
 
-def extract_data( dir ):
+def xml_extractor( dir ):
     DOMTree = parse( dir )
     collection = DOMTree.documentElement
     file_name_xml = collection.getElementsByTagName( 'filename' )[0]
     objects_xml = collection.getElementsByTagName( 'object' )
+    size_xml = collection.getElementsByTagName( 'size' )
 
     file_name = file_name_xml.childNodes[0].data
+
+    for size in size_xml:
+        width = size.getElementsByTagName( 'width' )[0]
+        height = size.getElementsByTagName( 'height' )[0]
+
+        width = width.childNodes[0].data
+        height = height.childNodes[0].data
 
     objects = []
     for object_xml in objects_xml:
@@ -26,10 +34,10 @@ def extract_data( dir ):
 
         objects.append( object )
 
-    return file_name, objects
+    return file_name, width, height, objects
 
 
 '''--------Test extract_labels--------'''
 if __name__ == '__main__':
-    file_name, objects = extract_data( '../data/VOCtest_06-Nov-2007/Annotations/000001.xml' )
-    print( file_name, '\n', objects )
+    file_name, width, height, objects = xml_extractor( '../data/VOCtest_06-Nov-2007/Annotations/000001.xml' )
+    print( file_name, '\n', width, '\n', height, '\n', objects )
