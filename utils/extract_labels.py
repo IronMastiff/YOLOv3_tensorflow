bc75cfc3bde2faf600793b93ec8ae693f28892ff
Iron Mastiff <1760298283@qq.com>
2018-04-11 16:36:18 +0800

    Add labels_normalizer

--- utils/extract_labels.py
+++ utils/extract_labels.py
@@ -1,5 +1,6 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
+import numpy as np
 
 def xml_extractor( dir ):
     DOMTree = parse( dir )
@@ -35,6 +36,76 @@ def xml_extractor( dir ):
         objects.append( object )
 
     return file_name, width, height, objects
+
+def labels_normaliszer( batches_filenames, target_width, target_height ):
+
+    class_map = {
+        'person' : 4,
+        'bird' : 5,
+        'cat' : 6,
+        'cow' : 7,
+        'dog' : 8,
+        'horse' : 9,
+        'sheep' : 10,
+        'aeroplane' : 11,
+        'bicycle' : 12,
+        'boat' : 13,
+        'bus' : 14,
+        'car' : 15,
+        'motorbike' : 16,
+        'train' : 17,
+        'bottle' : 18,
+        'chair' : 19,
+        'dining table' : 20,
+        'potted plant': 21,
+        'sofa' : 22,
+        'tv/monitor' : 23
+    }
+
+    batches_labels = []
+    batches_height_width = []
+    batch_labels = []
+    batch_height_width = []
+    labels = []
+    height_width = []
+    for batch_filenames in batches_filenames:
+        for filename in batch_filenames:
+            _, width, height, objects = xml_extractor( filename )
+            width_proprotion = 1.0 * target_width / width
+            height_proprotion = 1.0 * target_height / height
+
+            label = np.zeros( [target_height, target_width, 255] )
+            for object in objects:
+                class_label = class_map[object[0]]
+                xmin = object[1]
+                ymin = object[2]
+                xmax = object[3]
+                ymax = object[4]
+
+                x = ( 1.0 * xmax + xmin ) / 2 * width_proprotion
+                y = ( 1.0 * ymax + ymin ) / 2 * height_proprotion
+
+                bdbox_width = ( 1.0 * xmax - xmin ) * width_proprotion
+                bdbox_height = ( 1.0 * ymax - ymin ) * height_proprotion
+
+                label[int( y )][int( x )][0] = x    # Point x
+                label[int( y )][int( x )][1] = y    # Point y
+                label[int( y )][int( x )][2] = bdbox_width    # bdbox width
+                label[int( y )][int( x )][3] = bdbox_height    # bdbox_height
+                label[int( y )][int( x )][4] = 0.5    # objectness
+                label[int( y )][int( x )][class_label] = 0.9    # class label
+
+                labels.append( label )
+            batch_labels.append( labels )
+        batches_labels.append( batch_labels )
+
+        batches_labels = np.array( batches_labels )
+
+    return batches_labels
+
+
+
+
 
 
 '''--------Test extract_labels--------'''
