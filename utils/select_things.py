fe2a8fe3376825f4bd26fecf345a179476444c63
Iron Mastiff <1760298283@qq.com>
2018-04-12 13:53:07 +0800

    Add a function to select the checkpoint

---
+++ utils/select_things.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+
+
+def select_scale(tag, image_width, image_height):
+    if tag == 1:
+        scale_width = image_width / np.power(2, 5)
+        scale_height = image_height / np.power(2, 5)
+    if tag == 2:
+        scale_width = image_width / np.power(2, 4)
+        scale_height = image_height / np.power(2, 4)
+    if tag == 3:
+        scale_width = image_width / np.power(2, 3)
+        scale_height = image_height / np.power(2, 3)
+
+    return scale_width, scale_height
+
+
+def select_checkpoint(tag):
+    if not (os.path.exists('./models')):
+        os.makedirs('./models')
+
+    dir_name = 'scale' + str(tag)
+
+    checkpoint_path = os.path.join('./models', dir_name)
+
+    return checkpoint_path
