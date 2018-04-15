66edf1cd5ba686c43e1523125acb5a55717372ca
Iron Mastiff <1760298283@qq.com>
2018-04-15 15:36:03 +0800

    commit

--- eval.py
+++ eval.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 import os
 import argparse
 import cv2
+import matplotlib.pyplot as plt
 
 import reader
-from utils import net, read_config, select_things
+from utils import net, read_config, select_things, eval_uitls
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -16,14 +17,54 @@ def main( FLAGS ):
     if not os.path.exists( FLAGS.save_dir ):
         os.makedirs( FLAGS.save_dir )
 
-
     input_image = reader.get_image( FLAGS.image_dir, FLAGS.image_width, FLAGS.image_height )
+    output_image = np.copy( input_image )
 
     '''--------net--------'''
-    pre_scale1, pre_scale2, pre_scale3 = net.feature_extractor( input_iamge )
+    pre_scale1, pre_scale2, pre_scale3 = net.feature_extractor( input_image )
     scale1, scale2, scale3 = net.scales( pre_scale1, pre_scale2, pre_scale3 )
 
+    with tf.Session() as sess:
+        start_time = time.time()
+        saver = tf.train.Saver()
+        save_path = select_things.select_checkpoint( FLAGS.scale )
+        last_checkpoint = tf.train.latest_checkpoint( save_path, 'checkpoint' )
+        if last_checkpoint:
+            saver.restore(sess, last_checkpoint)
+            print( 'Success load model from{}', format( last_checkpoint ) )
+        else:
+            print( 'Model has not trained' )
 
+        scale1, scale2, scale3 = sess.run( [scale1, scale2, scale3] )
+
+    if FLAGS.scale == 1:
+        scale = scale1
+    if FLAGS.scale == 2:
+        scale = scale2
+    if FLAGS.scale == 3:
+        scale = scale3
+
+    boxes_labels = eval_uitls.label_extractor( scale )
+
+    bdboxes = eval_uitls.get_bdboxes( boxes_labels )
+
+    for bdbox in bdboxes:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        output_image = cv2.rectangle( output_image,
+                                      ( bdbox[0] - bdbox[2] / 2, bdbox[1] - bdbox[3] / 2 ),
+                                      ( bdbox[0] + bdbox[2] / 2, bdbox[1] + bdbox[3] / 2 ),
+                                      ( 200, 0, 0 ),
+                                      1 )
+        output_image = cv2.putText( output_image, bdbox[5],
+                                    ( bdbox[0] - bdbox[2] / 2, bdbox[1] - bdbox[3] / 2 ),
+                                    1.2,
+                                    (0, 255, 0),
+                                    2 )
+
+    
+
+    plt.imshow( output_image )
+    plt.show()
 
 
 
